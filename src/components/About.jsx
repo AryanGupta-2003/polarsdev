25c90e06604e64b80d11940fbf8173f6eebbad13
Domnic-Huter <aryanguptalko9@gmail.com>
2023-04-10 00:57:42 +0530

    added cards and animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ import { useAnimation } from 'framer-motion';
 const About = () =>{
 
     const {ref,inView}=useInView({
-        threshold: 0.7
+        threshold: 0.5
     });
     const head = useAnimation();
     const title = useAnimation();
@@ -20,43 +20,48 @@ const About = () =>{
             head.start({
                 x:0,
                 transition:{
-                    type: 'spring' , duration:1,bounce:0.3
+                    type: 'spring' , duration:4,bounce:0.15
                 }
             });
             title.start({
                 x:0,
                 transition:{
-                    type: 'spring' , duration:1,bounce:0.3
+                    type: 'spring' , duration:4,bounce:0.15
                 }
             });
             card_left.start({
                 x:0,
                 transition:{
-                    type: 'spring' , duration:1,bounce:0.3
+                    type: 'spring' , duration:4,bounce:0.15
                 }
             });
             card_right.start({
                 x:0,
                 transition:{
-                    type: 'spring' , duration:1,bounce:0.3
+                    type: 'spring' , duration:4,bounce:0.15
                 }
             });
             button.start({
-                x:0,
+                y:0,
                 transition:{
-                    type: 'spring' , duration:1,bounce:0.3
+                    type: 'spring' , duration:4,bounce:0.15
                 }
             });
             divider.start({
-                x:0,
+                y:0,
                 transition:{
-                    type: 'spring' , duration:1,bounce:0.3
+                    type: 'spring' , duration:4,bounce:0.15
                 }
             });
 
         }
         if(!inView){
-            head.start({x:'-100vw'})
+            head.start({x:'-200vw'});
+            title.start({x:'200vw'});
+            divider.start({y:'-200vw'});
+            button.start({y:'200vw'});
+            card_right.start({x:'200vw'});
+            card_left.start({x:'-200vw'});
         }
         console.log(inView);
     },[inView]);
@@ -68,25 +73,25 @@ const About = () =>{
             <motion.div className="content-heading" animate={head}>
             ABOUT POLARSDEV
             </motion.div>
-                <div className="content-catch">PASSIONATELY CREATIVE</div>
-                <div className="content-para"><div className="para-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste recusandae obcaecati aliquid perspiciatis dolor. Nulla placeat exercitationem incidunt adipisci fugiat, modi, veniam consectetur rerum quia esse iusto quae, accusamus voluptate neque reiciendis quaerat totam impedit pariatur ducimus odio alias facere consequuntur. Quam ea officia iste omnis eveniet velit dolores dolore molestias? Distinctio nisi amet illo culpa consequatur blanditiis, ratione, commodi ab necessitatibus accusamus earum repudiandae consequuntur debitis, repellendus tempora itaque.</div></div>
+                <motion.div className="content-catch" animate={title}>PASSIONATELY CREATIVE</motion.div>
+                <motion.div className="content-para" animate={title}><div className="para-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste recusandae obcaecati aliquid perspiciatis dolor. Nulla placeat exercitationem incidunt adipisci fugiat, modi, veniam consectetur rerum quia esse iusto quae, accusamus voluptate neque reiciendis quaerat totam impedit pariatur ducimus odio alias facere consequuntur. Quam ea officia iste omnis eveniet velit dolores dolore molestias? Distinctio nisi amet illo culpa consequatur blanditiis, ratione, commodi ab necessitatibus accusamus earum repudiandae consequuntur debitis, repellendus tempora itaque.</div></motion.div>
 
                 <div class="card-container">
-                    <div class="card">
+                    <motion.div class="card" animate={card_left}>
                         <h2 className="card-heading">BACKGROUND</h2>
                         <p className="card-para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ipsum voluptatem unde laboriosam iure suscipit vero minus, nemo architecto, facere asperiores labore facilis perspiciatis quidem corrupti natus maiores eveniet eum. Numquam sint velit minima in illo exercitationem omnis, veniam odio facere, totam ipsam impedit voluptatibus. Corporis nobis possimus at dolor.</p>
-                    </div>
-                    <span className="divider"></span>
-                    <div class="card">
+                    </motion.div>
+                    <motion.span animate={divider} className="divider"></motion.span>
+                    <motion.div class="card" animate={card_right}>
                         <h2 className="card-heading">GOALS</h2>
                         <p className="card-para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quas placeat minima corporis quae tenetur nemo earum optio, corrupti quibusdam obcaecati! Maiores, commodi. Cum recusandae tempore iure ex blanditiis eaque?</p>
                         
-                    </div>
+                    </motion.div>
                     
                 </div>
-                <button className="btn btn-darken btn-inline btn-about">
+                <motion.button className="btn btn-darken btn-inline btn-about" animate={button}>
                         Our Projects<i className="bx bx-right-arrow-alt"></i>
-                    </button>
+                    </motion.button>
             </div>
         </div>
         );
