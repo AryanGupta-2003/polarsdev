5405105acff2f5562caa873cdbf4bb701782dcf5
Domnic-Huter <aryanguptalko9@gmail.com>
2023-04-14 23:44:25 +0530

    testimonial corousel added

--- src/components/Testi.jsx
+++ src/components/Testi.jsx
@@ -1,7 +1,68 @@
+import React,{Component} from 'react';
+import "../pages/testi.css";
+import { motion } from "framer-motion"
+import {useInView} from 'react-intersection-observer'
+import { useEffect } from 'react';
+import { useAnimation } from 'framer-motion';
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import quote from "../assets/img/quote.png";
 
 const Testi = () =>{
     return (
-        <h1>Testi</h1>
+        <div className='testi-section'>
+        <div className='team-title'  >THE TEAM</div>
+        <div className='team-catch' >OUR DEVELOPEMENT WIZARDS</div>
+        <Carousel
+        showArrows={false}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        interval={3050}
+      >
+        <div>
+          
+          <div className="myCarousel">
+            <img className='icon-quote' src={quote}></img>
+            <p className='testi-para'>
+              It's freeing to be able to catch up on customized news and not be
+              distracted by a social media element on the same site
+            </p>
+          </div>
+          <img className='testi-img' src="https://wallpapercave.com/wp/wp4975694.jpg" />
+          <h3 className='testi-name'>randi raju</h3>
+            <h4 className='testi-desg'>Coder</h4>
+        </div>
+
+        <div>
+        <div className="myCarousel">
+        <img className='icon-quote' src={quote}></img>
+            <p className='testi-para'>
+              It's freeing to be able to catch up on customized news and not be
+              distracted by a social media element on the same site
+            </p>
+          </div>
+          <img className='testi-img' src="https://wallpapercave.com/wp/wp4975694.jpg" />
+          <h3 className='testi-name'>randi raju</h3>
+            <h4 className='testi-desg'>Coder</h4>
+        </div>
+
+        <div>
+        <div className="myCarousel">
+        <img className='icon-quote' src={quote}></img>
+            <p className='testi-para'>
+              It's freeing to be able to catch up on customized news and not be
+              distracted by a social media element on the same site
+            </p>
+          </div>
+          <img className='testi-img' src="https://wallpapercave.com/wp/wp4975694.jpg" />
+          <h3 className='testi-name'>randi raju</h3>
+            <h4 className='testi-desg'>Coder</h4>
+        </div>
+      </Carousel>
+      </div>
         );
     }
     
