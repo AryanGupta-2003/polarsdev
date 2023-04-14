a152a9712ffee4a825fca2b957091322784746b5
Domnic-Huter <aryanguptalko9@gmail.com>
2023-04-14 23:44:40 +0530

    added footer and related css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,18 @@
+import React from "react";
+import "../pages/footer.css";
 const Footer = () =>{
     return (
-        <h1>footer</h1>
+           <div className="footer-section">
+                <div className="footer-title">PolarsDev</div>
+                <div className="footer-catch">Your polaris for developement</div>
+                 <div className="footer-social">
+                    <a href="#" title=""><i className="bx bxl-facebook footer-social-button footer-social-fb"></i></a>
+                    <a href="#" title="" ><i className="bx bxl-instagram footer-social-button footer-social-in"><div className="footer-ins-bg"></div></i></a>
+                    <a href="#" title="" ><i className="bx bxl-linkedin  footer-social-button footer-social-tw"></i></a>
+                 </div> 
+                <div className="footer-divider"></div>
+                <div className="footer-copyright">copyright 2023 © All rights reserved by PolarsDev</div>
+           </div> 
         );
     }
     
